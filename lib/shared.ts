22fff5403664d68efc4c89979bdfb9b4c4a763e0
Tatiana Yeremenko <tyeremenko@goguardian.com>
2024-05-30 15:54:48 -0700

    Update shared.ts

--- lib/shared.ts
+++ lib/shared.ts
@@ -1,3 +1,3 @@
-export const domain = process.env.DOMAIN ?? "testing.tutorme.com";
+export const domain = process.env.DOMAIN ?? "stg-tutor.peardeck.com";
 export const product = process.env.PLAYWRIGHT_PRODUCT ?? "chrome";
 export const verbose = process.env.VERBOSE === "true" || process.env.DEBUG === "true";
