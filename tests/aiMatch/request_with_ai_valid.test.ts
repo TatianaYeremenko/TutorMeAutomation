f6bbc65e7053d50276155153501c9152cd2276b2
Tatiana Yeremenko <tyeremenko@goguardian.com>
2024-08-23 14:09:57 -0700

    Update request_with_ai_valid.test.ts

--- tests/aiMatch/request_with_ai_valid.test.ts
+++ tests/aiMatch/request_with_ai_valid.test.ts
@@ -1,112 +1,102 @@
+it.only("Match Pal is available", async () => {
+  const { struct, page } = await createVisitor();
+  // await page.goto('https://aiasstv2.trial.tutorme.dev/sign-in/');
 
-it("request with AI Match Pal", async () => {
+  //connect with a tutor
+  // sign in
+  await struct.authPages.signIn.email.waitForVisible();
+  await struct.authPages.signIn.email.fill("ai-user@local.tutor.peardeck.com");
 
-    const { struct, page } = await createVisitor();
-    // await page.goto('https://aiasstv2.trial.tutorme.dev/sign-in/');
+  await struct.authPages.signIn.password.waitForVisible();
+  await struct.authPages.signIn.password.type("AI@go!");
 
-      //connect with a tutor
-    // sign in
-    await struct.authPages.signIn.email.waitForVisible();
-    await struct.authPages.signIn.email.fill("ai-user@local.tutor.peardeck.com");
+  await page.waitForTimeout(2000);
+  await fillRecaptcha(struct.authPages.signIn.recaptcha);
+  await page.waitForTimeout(1000);
 
-    await struct.authPages.signIn.password.waitForVisible();
-    await struct.authPages.signIn.password.type("AI@go!");
+  await struct.authPages.signIn.signIn.waitForVisible();
+  await struct.authPages.signIn.signIn.click();
+  await page.waitForTimeout(1000);
 
-    await page.waitForTimeout(2000);
-    await fillRecaptcha(struct.authPages.signIn.recaptcha);
-    await page.waitForTimeout(1000);
+  await struct.homepage.requestATutor.click();
+  await page.waitForTimeout(1000);
 
-    await struct.authPages.signIn.signIn.waitForVisible();
-    await struct.authPages.signIn.signIn.click();
-
-    await page.waitForTimeout(2000);
-
-    await page.getByRole("link", { name: "Request a live tutor" }).click();
-    await page
-    .getByRole("heading", {
-      name: "What subject area do you need help with?",
-    })
-    .isVisible(); 
-
-  await page.locator('label').filter({ hasText: '6th grade' }).click();
+  await page.locator("label").filter({ hasText: "6th grade" }).click();
   await struct.sessionRequest.nextArrow.click();
 
-  await page.locator('label').filter({ hasText: 'Math' }).click();
+  await page.locator("label").filter({ hasText: "Math" }).click();
   await struct.sessionRequest.nextArrow.click();
 
-  await page.locator('label').filter({ hasText: 'Basic Math' }).click();
+  await page.locator("label").filter({ hasText: "Basic Math" }).click();
   await struct.sessionRequest.nextArrow.click();
 
-  await page.getByTestId('sessionRequest.description').click();
-  await page.getByTestId('sessionRequest.description').fill('If y(x-1)=z then x=');
-  await page.waitForTimeout(5000);
+  await page.getByTestId("sessionRequest.description").click();
+  await page
+    .getByTestId("sessionRequest.description")
+    .fill("If y(x-1)=z then x=");
+  await page.waitForTimeout(2000);
+
+  // Match Pal is available
   await struct.sessionRequest.refineSearch.waitForVisible();
-  await struct.sessionRequest.refineSearch.click();
-  await page.waitForTimeout(5000);
+  await struct.sessionRequest.matchPalCTA.waitForVisible();
+  await page.waitForTimeout(1000);
+  await struct.sessionRequest.nextArrow.click();
 
-  await struct.modals.matchPal.waitForVisible();
-  await struct.modals.matchPal.content.close.waitForVisible();
+  // await s.page.locator('//div/p[contains(text(),"I am so lost")]').clear();
+  await page.locator("label").filter({ hasText: "I am so lost" }).click();
 
-  //another question
-  await page.getByTestId('modals.matchPal.content.response').fill('understanding how to find x');
-  await page.getByTestId('modals.matchPal.content.response').press('Enter');
-  await page.waitForTimeout(5000);
+  await page.locator("label").filter({ hasText: "Audio only" }).click();
+  await struct.sessionRequest.nextArrow.click();
 
-  //one more question
-  await page.getByTestId('modals.matchPal.content.response').fill('yes');
-  await page.getByTestId('modals.matchPal.content.response').press('Enter');
-  await page.waitForTimeout(5000);
+  // move to the confirmation page
+  await struct.sessionRequest.codeOfConduct.click();
+  await struct.sessionRequest.requestTutor.click();
+  await page.waitForTimeout(1000);
 
-  await struct.modals.matchPal.content.confirmGoals.click();
-  await page.waitForTimeout(5000);
+  await struct.modals.notifyingTutors.content.cancel.click();
+  await struct.modals.confirmCancel.content.cancel.click();
 
-  await struct.modals.matchPal.content.continueRequest.click();
-  await page.waitForTimeout(5000);
-
-  await struct.sessionRequest.sessionGoals.waitForVisible();
-//   console.log(await struct.sessionRequest.sessionGoals.text());
+  //click on my Account and Sign Out
+  await struct.header.userTools.openMenu.click();
+  await struct.userMenu.signOut.click();
 });
 
 it("student should be able to continue with the request after five attempts", async () => {
+  const { struct, page } = await createVisitor();
+  // await page.goto('https://aiasstv2.trial.tutorme.dev/sign-in/');
 
-    const { struct, page } = await createVisitor();
-    // await page.goto('https://aiasstv2.trial.tutorme.dev/sign-in/');
+  //connect with a tutor
+  // sign in
+  await struct.authPages.signIn.email.waitForVisible();
+  await struct.authPages.signIn.email.fill("ai-user@local.tutor.peardeck.com");
 
-      //connect with a tutor
-    // sign in
-    await struct.authPages.signIn.email.waitForVisible();
-    await struct.authPages.signIn.email.fill("ai-user@local.tutor.peardeck.com");
+  await struct.authPages.signIn.password.waitForVisible();
+  await struct.authPages.signIn.password.type("AI@go!");
 
-    await struct.authPages.signIn.password.waitForVisible();
-    await struct.authPages.signIn.password.type("AI@go!");
+  await page.waitForTimeout(2000);
+  await fillRecaptcha(struct.authPages.signIn.recaptcha);
+  await page.waitForTimeout(1000);
 
-    await page.waitForTimeout(2000);
-    await fillRecaptcha(struct.authPages.signIn.recaptcha);
-    await page.waitForTimeout(1000);
+  await struct.authPages.signIn.signIn.waitForVisible();
+  await struct.authPages.signIn.signIn.click();
+  await page.waitForTimeout(1000);
 
-    await struct.authPages.signIn.signIn.waitForVisible();
-    await struct.authPages.signIn.signIn.click();
+  await struct.homepage.requestATutor.click();
+  await page.waitForTimeout(1000);
 
-    await page.waitForTimeout(2000);
-
-    await page.getByRole("link", { name: "Request a live tutor" }).click();
-    await page
-    .getByRole("heading", {
-      name: "What subject area do you need help with?",
-    })
-    .isVisible(); 
-
-  await page.locator('label').filter({ hasText: '6th grade' }).click();
+  await page.locator("label").filter({ hasText: "6th grade" }).click();
   await struct.sessionRequest.nextArrow.click();
 
-  await page.locator('label').filter({ hasText: 'Math' }).click();
+  await page.locator("label").filter({ hasText: "Math" }).click();
   await struct.sessionRequest.nextArrow.click();
 
-  await page.locator('label').filter({ hasText: 'Basic Math' }).click();
+  await page.locator("label").filter({ hasText: "Basic Math" }).click();
   await struct.sessionRequest.nextArrow.click();
 
-  await page.getByTestId('sessionRequest.description').click();
-  await page.getByTestId('sessionRequest.description').fill('If y(x-1)=z then x=');
+  await page.getByTestId("sessionRequest.description").click();
+  await page
+    .getByTestId("sessionRequest.description")
+    .fill("If y(x-1)=z then x=");
   await page.waitForTimeout(5000);
   await struct.sessionRequest.refineSearch.waitForVisible();
   await struct.sessionRequest.refineSearch.click();
@@ -117,24 +107,23 @@ it("student should be able to continue with the request after five attempts", as
 
   //try 5 times
   for (let i = 0; i < 5; i++) {
-  await page.getByTestId('modals.matchPal.content.response').fill('no');
-  await page.getByTestId('modals.matchPal.content.response').press('Enter');
-  await page.waitForTimeout(2000);
+    await page.getByTestId("modals.matchPal.content.response").fill("no");
+    await page.getByTestId("modals.matchPal.content.response").press("Enter");
+    await page.waitForTimeout(2000);
   }
-  //last message 
+  //last message
   await struct.modals.matchPal.content.message(10).text();
   await page.waitForTimeout(5000);
 
   await struct.modals.matchPal.content.continueRequest.click();
   await page.waitForTimeout(5000);
-  
+
   // session goals should not be dispalyed
   await struct.sessionRequest.sessionGoals.waitForHidden();
 
-  // still visiable 
+  // still visiable
   await struct.sessionRequest.matchPalCTA.waitForVisible();
   await struct.sessionRequest.matchPalCTA.click();
   await struct.modals.matchPal.content.continueRequest.click();
   await struct.sessionRequest.close.click();
-
 });
