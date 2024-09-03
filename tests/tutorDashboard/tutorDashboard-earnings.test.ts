995ff2b3de45f519217ddfb7fd3fd88e2a683559
Tatiana Yeremenko <tyeremenko@goguardian.com>
2024-09-02 21:27:57 -0700

    Update and rename tutorDashboard-earnings.fixneeded.ts to tutorDashboard-earnings.test.ts

--- tests/tutorDashboard/tutorDashboard-earnings.test.ts
+++ tests/tutorDashboard/tutorDashboard-earnings.test.ts
@@ -1,14 +1,8 @@
-
-it("tutor can able to see the message : No earnings to report ", async () => {
-  
+it.only("tutor can able to see the message : No earnings to report ", async () => {
   //create tutor
-  const {
-      struct,
-      page,
-      user
-  } = await createQaUser('tutor');
+  const { struct, page, user } = await createQaTutor();
 
-  await page.waitForTimeout(1000);
+  await page.waitForTimeout(2000);
 
   // click on Background Check
   await struct.tutorDashboard.header.earnings.waitForVisible();
@@ -16,34 +10,33 @@ it("tutor can able to see the message : No earnings to report ", async () => {
   await struct.tutorDashboard.header.earnings.click();
 
   // check url
-  expect(page.url()).toContain('/earnings/');
+  expect(page.url()).toContain("/earnings/");
 
   // check the earning
   await (await page.waitForSelector('text="current week"')).textContent();
   await page.waitForSelector('text="all time"');
 
+  // check the earning
+  await struct.tutorDashboard.earnings.currentWeekAmount.waitForVisible();
+  await struct.tutorDashboard.earnings.cumulativeAmount.waitForVisible();
+  await struct.tutorDashboard.earnings.info.waitForVisible();
 
-    //tutor signs out
-    await struct.tutorDashboard.header.userTools.username.click();
-    await struct.userMenu.signOut.click();
-  
+  //tutor signs out
+  await struct.tutorDashboard.header.userTools.username.click();
+  await struct.userMenu.signOut.click();
 });
 
 it("tutor can able to see the current week and total earnings", async () => {
-
   //create tutor
-  const {
-      struct,
-      page
-  } = await createVisitor();
+  const { struct, page } = await createVisitor();
 
   // sign in
   await struct.authPages.signIn.email.waitForVisible();
-  await struct.authPages.signIn.email.fill('tatiana.v.yeremenko@gmail.com');
+  await struct.authPages.signIn.email.fill("tatiana.v.yeremenko@gmail.com");
 
   await struct.authPages.signIn.password.waitForVisible();
-  await struct.authPages.signIn.password.fill('Ff22558800!');
-  await page.waitForTimeout(500);
+  await struct.authPages.signIn.password.type("Ff22558800!");
+  await page.waitForTimeout(1000);
 
   await struct.authPages.signIn.recaptcha.waitForVisible();
   await fillRecaptcha(struct.authPages.signIn.recaptcha);
@@ -51,13 +44,12 @@ it("tutor can able to see the current week and total earnings", async () => {
 
   await struct.authPages.signIn.signIn.waitForVisible();
   await struct.authPages.signIn.signIn.click();
-  await page.waitForTimeout(500);
+  await page.waitForTimeout(5000);
 
-  // //click on Cookies Banner     
-  // const cookieBanner = await page.waitForSelector('//button[contains(text(),"Accept Cookies")]');
-  // await cookieBanner.click();
- 
   // check the earning
+  await struct.tutorDashboard.header.availableTutoring.waitForVisible();
+  await struct.tutorDashboard.header.availableTutoring.click();
+
   await struct.tutorDashboard.header.earnings.waitForVisible();
   await struct.tutorDashboard.header.earnings.click();
 
@@ -69,5 +61,4 @@ it("tutor can able to see the current week and total earnings", async () => {
   //tutor signs out
   await struct.tutorDashboard.header.userTools.openMenu.click();
   await struct.userMenu.signOut.click();
-   
 });
