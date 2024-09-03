6ca39745b6ed1074f3ed677ac7f86b114e3e23ee
Tatiana Yeremenko <tyeremenko@goguardian.com>
2024-09-02 21:33:45 -0700

    Update tutorDashboard-part-time-tutor.fixneeded.ts

--- tests/tutorDashboard/tutorDashboard-part-time-tutor.fixneeded.ts
+++ tests/tutorDashboard/tutorDashboard-part-time-tutor.fixneeded.ts
@@ -1,55 +1,21 @@
-it("part tutor should not able to see earnings section", async () => {
+it("tutor can able to see the past lessons and wl", async () => {
+  //create tutor
+  const { struct, page, user } =  await createQaTutor();
   
-    //create tutor
-    const {
-        struct,
-        page
-    } = await createVisitor();
+  // click on Past Records
+  await struct.tutorDashboard.header.pastTutoring.waitForVisible();
+  await struct.tutorDashboard.header.pastTutoring.click();
 
-    // sign in
-    await struct.authPages.signIn.email.waitForVisible();
-    await struct.authPages.signIn.email.fill('part_time_tutor@local.tutorme.com');
-  
-    await struct.authPages.signIn.password.waitForVisible();
-    await struct.authPages.signIn.password.fill('Tutor12345!');
-    await page.waitForTimeout(500);
-  
-    await struct.authPages.signIn.recaptcha.waitForVisible();
-    await fillRecaptcha(struct.authPages.signIn.recaptcha);
-    await struct.authPages.signIn.recaptcha.waitForVisible();
+  // find lessons
+  await struct.tutorDashboard.pastLessons.findLessons.click();
 
-    await struct.authPages.signIn.signIn.waitForVisible();
-    await struct.authPages.signIn.signIn.click();
-    await page.waitForTimeout(2000);
+  // click on Past Records agian and check WL
+  await struct.tutorDashboard.header.pastTutoring.click();
+  await struct.tutorDashboard.pastLessons.pastWriting.click();
 
-    // await page.setViewportSize({
-    //     width: 1024,
-    //     height: 768,
-    //   });
+  await struct.tutorDashboard.pastWriting.findWriting.click();
 
-    await page.keyboard.press('PageDown');  
-
-
-    // check if the earning is not visible
-    await struct.tutorDashboard.header.earnings.waitForHidden();
-
-    // check if the earning is not visible
-    await struct.tutorDashboard.header.checkr.waitForHidden();
-        
-    // check if the everything elase is avalble
-    await struct.tutorDashboard.header.availableTutoring.waitForVisible();
-    await struct.tutorDashboard.header.resources.waitForVisible();
-    await struct.tutorDashboard.header.pastTutoring.waitForVisible();
-    await page.waitForTimeout(500);
-
-    // //click on Cookies Banner     
-    const cookieBanner = await page.waitForSelector('//button[contains(text(),"Accept Cookies")]');
-    await cookieBanner.click();
-
-    //tutor signs out
-    await struct.tutorDashboard.header.userTools.openMenu.click();
-    await struct.userMenu.signOut.click();
-    //tutor signs out
-    // await page.close();
-
+  //tutor signs out
+  await struct.tutorDashboard.header.userTools.openMenu.click();
+  await struct.userMenu.signOut.click();
 });
