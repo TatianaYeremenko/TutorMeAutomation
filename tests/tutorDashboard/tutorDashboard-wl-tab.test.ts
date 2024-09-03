0eca31c51d16fe3bc2c23006b6527a2137d26d3e
Tatiana Yeremenko <tyeremenko@goguardian.com>
2024-09-02 21:55:46 -0700

    Update and rename tutorDashboard-writingLab-disable.inprogress.ts to tutorDashboard-wl-tab.test.ts

---
+++ tests/tutorDashboard/tutorDashboard-wl-tab.test.ts
@@ -0,0 +1,54 @@
+it("tutor is able to see WL page", async () => {
+    //create tutor
+    const { struct, page, user } = await createQaTutor();
+  
+    await page.waitForTimeout(1000);
+    // click on Bell Available Tutoring
+    await struct.tutorDashboard.header.availableTutoring.waitForVisible();
+    await struct.tutorDashboard.header.availableTutoring.click();
+    await page.waitForTimeout(1000);
+  
+    // WL Tab
+    await page
+      .locator(
+        '//*[@id="react-app"]/div/div[4]/div/div/nav/div[2]/div[1]/div/a/span'
+      )
+      .isVisible();
+    await page
+      .locator(
+        '//*[@id="react-app"]/div/div[4]/div/div/nav/div[2]/div[1]/div/a/span'
+      )
+      .click();
+  
+    // header Writing Labs
+    await page.locator('//h2[contains(text(),"Writing Labs")]').isVisible();
+  
+    // Available Writing Lab requests
+    await page
+      .locator('//h3[contains(text(),"Available Writing Lab requests")]')
+      .isVisible();
+  
+    // There are no active Writing Lab requests
+    await page
+      .locator(
+        '//h3[contains(text(),"There are no active Writing Lab requests")]'
+      )
+      .isVisible();
+  
+    // This section will automatically update when a new request becomes available - no need to refresh your page.
+    await page
+      .locator(
+        '//p[contains(text(),"This section will automatically update when a new request becomes available - no need to refresh your page.")]'
+      )
+      .isVisible();
+  
+    // red dot is no visible for empty page
+    await page
+      .locator('//*[@id="react-app"]/div/div[4]/div/nav/div[2]/div[1]/div/a/div')
+      .isHidden();
+  
+    //tutor signs out
+    await struct.tutorDashboard.header.userTools.openMenu.click();
+    await struct.userMenu.signOut.click();
+  });
+  
